783dd118c69d67e71ebabf54c30455f41dfeaf9b
iindir <jonauv@gmail.com>
2020-10-25 00:49:16 +0800

    Touch support
    also laid foundation for menu

---
+++ breakOutWebMenu.js
@@ -0,0 +1,10 @@
+function viewMenu(){
+	document.getElementById("menuBtn").style.display = "none";
+	document.getElementById("menu").style.display = "block";
+	document.getElementById("resumeBtn").style.display = "block";
+}
+function hideMenu(){
+	document.getElementById("menuBtn").style.display = "block";
+	document.getElementById("menu").style.display = "none";
+	document.getElementById("resumeBtn").style.display = "none";
+}
